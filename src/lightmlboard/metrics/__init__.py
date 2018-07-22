0bf9d820093394bc23df949e914dde22a3cc795f
xavier dupré <xavier.dupre@gmail.com>
2018-07-22 20:53:16 +0200

    fix style

--- src/lightmlboard/metrics/__init__.py
+++ src/lightmlboard/metrics/__init__.py
@@ -2,7 +2,7 @@
 @file
 @brief Implements metrics.
 """
-import sklearn.metrics as metrics
+import sklearn.metrics as skmetrics
 from .classification import roc_auc_score_micro, roc_auc_score_macro, reshape, multi_label_jaccard
 from .regression import mse
 from .regression_custom import l1_reg_max
@@ -22,8 +22,8 @@ def sklearn_metric(met, exp, val):
         raise TypeError("val must be a container of floats")
     if isinstance(exp, str):
         raise TypeError("exp must be a container of floats")
-    if hasattr(metrics, met):
-        f = getattr(metrics, met)
+    if hasattr(skmetrics, met):
+        f = getattr(skmetrics, met)
         exp, val = reshape(exp, val)
         return f(exp, val)
     else:
