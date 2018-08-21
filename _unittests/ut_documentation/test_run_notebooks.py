19932b036958affbdebd85903da97d5066c99f5c
xavier dupré <xavier.dupre@gmail.com>
2018-08-21 14:45:49 +0200

    add local job jenkins linux

--- _unittests/ut_documentation/test_run_notebooks.py
+++ _unittests/ut_documentation/test_run_notebooks.py
@@ -9,8 +9,6 @@ import unittest
 from pyquickhelper.loghelper import fLOG
 from pyquickhelper.pycode import get_temp_folder, ExtTestCase
 from pyquickhelper.ipythonhelper import execute_notebook_list, execute_notebook_list_finalize_ut
-from pyquickhelper.pycode import is_travis_or_appveyor
-from pyquickhelper.ipythonhelper import install_python_kernel_for_unittest
 
 try:
     import src
@@ -36,13 +34,6 @@ class TestRunNotebooksPython(ExtTestCase):
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
 
-        if sys.version_info[0] == 2:
-            # notebooks are not converted into python 2.7, so not tested
-            return
-
-        kernel_name = None if is_travis_or_appveyor() else install_python_kernel_for_unittest(
-            "lightmlboard")
-
         temp = get_temp_folder(__file__, "temp_run_notebooks")
 
         # selection of notebooks
@@ -58,12 +49,6 @@ class TestRunNotebooksPython(ExtTestCase):
 
         # function to tell that a can be run
         def valid(cell):
-            if "open_html_form" in cell:
-                return False
-            if "open_window_params" in cell:
-                return False
-            if '<div style="position:absolute' in cell:
-                return False
             return True
 
         # additionnal path to add
@@ -77,7 +62,7 @@ class TestRunNotebooksPython(ExtTestCase):
 
         # run the notebooks
         res = execute_notebook_list(
-            temp, keepnote, fLOG=fLOG, valid=valid, additional_path=addpaths, kernel_name=kernel_name)
+            temp, keepnote, fLOG=fLOG, valid=valid, additional_path=addpaths)
         execute_notebook_list_finalize_ut(
             res, fLOG=fLOG, dump=src.lightmlboard)
 
